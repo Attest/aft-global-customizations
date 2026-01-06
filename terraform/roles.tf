cdba3a0ed7b4d9249b1a85149f34b9fa7fd86812
Nikita Maximov <nikita.maximov@askattest.com>
2026-01-06 10:25:47 +0000

    feat(oidc): add oidc provider for github actions

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -5,6 +5,7 @@ terraform {
 
 locals {
   tfc_org = "askattest"
+  gha_org = "Attest"
 }
 
 ############################
@@ -60,6 +61,51 @@ resource "aws_iam_role" "tfc_runner" {
 }
 
 ############################
+# OIDC provider for GHA
+############################
+resource "aws_iam_openid_connect_provider" "gha" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com", # Default audience in HCP Terraform for AWS.
+  ]
+}
+
+############################
+# IAM role GHA will assume (via OIDC)
+############################
+data "aws_iam_policy_document" "gha_runner_assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.gha.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${local.gha_org}/*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "gha_runner" {
+  name = "github-actions-deploy-role"
+  assume_role_policy = data.aws_iam_policy_document.gha_runner_assume_role_policy.json
+}
+
+
+############################
 # Permissions for the role
 # - bootstrap with AdministratorAccess (simple)
 ############################
@@ -67,5 +113,10 @@ resource "aws_iam_role" "tfc_runner" {
 # Option A (toggle via var.attach_admin_policy)
 resource "aws_iam_role_policy_attachment" "admin" {
   role       = aws_iam_role.tfc_runner.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "admin_gha" {
+  role       = aws_iam_role.gha_runner.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
