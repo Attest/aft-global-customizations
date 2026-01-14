785325f14b9ac9fde8c2194be67385ff74077e27
emmanuel.pius-ogiji <emmanuel.pius-ogiji@askattest.com>
2026-01-14 14:57:51 +0000

    feat: Add extra tags to AFT resources feat: Add IAM Account alias based on request template feat: Add buckets for vpc logs, load balancer access logs and s3 access logs

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+locals {
+  application = "AFT"
+}
+
+data "aws_ssm_parameter" "alias" {
+  name = "/aft/account-request/custom-fields/account_alias"
+}
+
+resource "aws_iam_account_alias" "alias" {
+  account_alias = data.aws_ssm_parameter.alias.value
+}
