29ce34447dbd029bcf242d81be6ad296dab101b9
emmanuel.pius-ogiji <emmanuel.pius-ogiji@askattest.com>
2026-01-15 11:54:48 +0000

    feat: Add ebs encryption by default and block public access settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,14 @@ data "aws_ssm_parameter" "alias" {
 resource "aws_iam_account_alias" "alias" {
   account_alias = data.aws_ssm_parameter.alias.value
 }
+
+resource "aws_ebs_encryption_by_default" "ebs" {
+  enabled = true
+}
+
+resource "aws_s3_account_public_access_block" "s3_public_access_block" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
